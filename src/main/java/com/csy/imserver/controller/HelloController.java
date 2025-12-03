f517c253215bf097599000c4e4d5bd52f20563c8
Clic <1499978484@qq.com>
2025-12-03 18:15:13 +0800

    更新工作流

--- src/main/java/com/csy/imserver/controller/HelloController.java
+++ src/main/java/com/csy/imserver/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.csy.imserver.controller;
 
+import com.csy.imserver.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,31 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        System.out.println(Thread.currentThread().toString());
+        System.out.println(Thread.currentThread().getName());
         return "你好！我是原生编译的 Java 程序。a运行在: " + System.getProperty("os.name");
     }
 
+
+    @GetMapping("/t1")
+    public User t1() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        // 使用反射创建对象，并赋值 返回
+        try {
+            Class<?> clazz = Class.forName("com.csy.imserver.entity.User");
+            User user = (User) clazz.newInstance();
+            user.setName("张三");
+            user.setPassword("<PASSWORD>");
+            user.setEmail("<EMAIL>");
+            user.setPhone("12345678901");
+            user.setAvatar("https://avatars.githubusercontent.com/u/10251060?s=200&v=4");
+            user.setSex("男");
+            user.setBirthday("1990-01-01");
+            return user;
+        } catch (Exception e) {
+            System.out.println("反射失败：" + e.getMessage());
+            return null;
+        }
+
+    }
 
 
 }
---
+++ src/main/java/com/csy/imserver/entity/User.java
@@ -0,0 +1,82 @@
+package com.csy.imserver.entity;
+
+
+public class User {
+    private String name;
+    private String password;
+    private String email;
+    private String phone;
+    private String avatar;
+    private String sex;
+    private String birthday;
+
+
+
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", sex='" + sex + '\'' +
+                ", birthday='" + birthday + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+}
